9eb551b4e2108c9f7a8a43be9002676a1109c906
Mattia Maio <mattia.maio10@gmail.com>
2021-11-17 16:57:37 +0100

    apportate modifiche alla selezione degli elementi

--- src/org/generation/italy/array/CibiPreferiti.java
+++ src/org/generation/italy/array/CibiPreferiti.java
@@ -4,7 +4,7 @@ public class CibiPreferiti {
 
 	public static void main(String[] args) {
 		
-		String[] favFood = new String [6];
+		String[] favFood = new String[6];
 		
 		favFood[0] = "Tortellini con brodo di carne";
 		favFood[1] = "Pizza margherita";		
@@ -13,14 +13,16 @@ public class CibiPreferiti {
 		favFood[4] = "Tagliata di carne";
 		favFood[5] = "Seppie alla griglia";
 		
-		System.out.println("Array length is: " + (favFood.length));
+		int lengthArray = favFood.length;
 		
-		System.out.println("My favourite food is: " + favFood[0]);
-		System.out.println("My least favourite food is: " + favFood[5]);
+		System.out.println("My favourite food list is " + lengthArray + " foods long");
 		
-		System.out.println("My third favourite food is: " + favFood[2]);
-	
-	
+		System.out.println("\nMy favourite food is: " + favFood[0]);
+		
+		System.out.println("\nMy least favourite food is: " + favFood[lengthArray -1]); //-1 è la posizione dell'ultimo elemento dell'array
+		
+		System.out.println("\nMy third favourite food is: " + (favFood[(lengthArray -1) / 2])); // posizione -1 /2 divide l'intero array a metà, partendo dalla conoscenza dell'ultimo elemento e seleziona l'elemento mediano
+
 	}
 
 }
